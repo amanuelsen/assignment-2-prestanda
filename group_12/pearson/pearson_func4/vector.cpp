6adc9bd9d8599de35483820e930fa6140c0d3287
Filip Dennryd <filip.dennryd@gmail.com>
2025-10-16 13:18:53 +0200

    optimized analysis folder with references, less copy constructor usage

---
+++ group_12/pearson/pearson_func4/vector.cpp
@@ -0,0 +1,161 @@
+/*
+Author: David Holmqvist <daae19@student.bth.se>
+*/
+
+#include "vector.hpp"
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+// used for SIMD optimization
+#include <immintrin.h>
+
+Vector::Vector()
+    : size{0}, data{nullptr}
+{
+}
+
+Vector::~Vector()
+{
+    if (data)
+    {
+        delete[] data;
+    }
+
+    size = 0;
+}
+
+Vector::Vector(unsigned size)
+    : size{size}, data{new double[size]}
+{
+}
+
+Vector::Vector(unsigned size, double *data)
+    : size{size}, data{data}
+{
+}
+
+Vector::Vector(const Vector &other)
+    : Vector{other.size}
+{
+    for (auto i{0}; i < size; i++)
+    {
+        data[i] = other.data[i];
+    }
+}
+
+unsigned Vector::get_size() const
+{
+    return size;
+}
+
+double *Vector::get_data()
+{
+    return data;
+}
+
+double Vector::operator[](unsigned i) const
+{
+    return data[i];
+}
+
+double &Vector::operator[](unsigned i)
+{
+    return data[i];
+}
+
+
+// SIMD optimization
+double Vector::mean() const
+{
+    double result = 0.0;
+    int i = 0;
+
+    // use AVX to process 4 doubles at a time
+    __m256d acc = _mm256_setzero_pd();
+
+    // increments of 4
+    for (; i <= size - 4; i += 4)
+    {
+        __m256d v = _mm256_loadu_pd(&data[i]);
+        acc = _mm256_add_pd(acc, v);
+    }
+
+    // horizontally add the 4 elements in acc
+    alignas(32) double temp[4];
+    _mm256_store_pd(temp, acc);
+    result = temp[0] + temp[1] + temp[2] + temp[3];
+
+    // handle remaining elements (outside the step of 4)
+    for (; i < size; ++i)
+    {
+        result += data[i];
+    }
+
+    return result / static_cast<double>(size);
+}
+
+double Vector::magnitude() const
+{
+    auto dot_prod{dot(*this)};
+    return std::sqrt(dot_prod);
+}
+
+// no noticed difference with SIMD
+Vector Vector::operator/(double div)
+{
+    auto result{*this};
+
+    for (auto i{0}; i < size; i++)
+    {
+        result[i] /= div;
+    }
+
+    return result;
+}
+
+// no noticed difference with SIMD
+Vector Vector::operator-(double sub)
+{
+    auto result{*this};
+
+    for (auto i{0}; i < size; i++)
+    {
+        result[i] -= sub;
+    }
+
+    return result;
+}
+
+// SIMD optimization
+double Vector::dot(const Vector& rhs) const
+{
+    double result = 0.0;
+    int i = 0;
+
+    // use AVX to process 4 doubles at a time
+    __m256d acc = _mm256_setzero_pd();
+
+    // increments of 4
+    for (; i <= size - 4; i += 4)
+    {
+        __m256d a = _mm256_loadu_pd(&data[i]);
+        __m256d b = _mm256_loadu_pd(&rhs.data[i]);
+        // store product in prod and add to acc
+        __m256d prod = _mm256_mul_pd(a, b);
+        acc = _mm256_add_pd(acc, prod);
+    }
+
+    // horizontally add the 4 elements in acc
+    alignas(32) double temp[4];
+    _mm256_store_pd(temp, acc);
+    result = temp[0] + temp[1] + temp[2] + temp[3];
+
+    // handle remaining elements if any exists (outside the step of 4)
+    for (; i < size; ++i)
+    {
+        result += data[i] * rhs.data[i];
+    }
+
+    return result;
+} 
