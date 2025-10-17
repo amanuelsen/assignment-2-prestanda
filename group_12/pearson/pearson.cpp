e12d8803b9744c95a2a4ef827745a8f34730ae5d
Filip Dennryd <filip.dennryd@gmail.com>
2025-10-17 20:11:34 +0200

    cleaned the code

--- group_12/pearson/pearson.cpp
+++ group_12/pearson/pearson.cpp
@@ -10,15 +10,13 @@ Author: David Holmqvist <daae19@student.bth.se>
 int main(int argc, char const* argv[])
 {
     if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " [thread_count] [dataset] [outfile]" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " [dataset] [outfile]" << std::endl;
         std::exit(1);
     }
 
-    auto datasets { Dataset::read(argv[2]) };
-    int thread_count = std::stoi(argv[1]);
-    
+    auto datasets { Dataset::read(argv[1]) };
     auto corrs { Analysis::correlation_coefficients(datasets) };
-    Dataset::write(corrs, argv[3]);
+    Dataset::write(corrs, argv[2]);
 
     return 0;
 }
