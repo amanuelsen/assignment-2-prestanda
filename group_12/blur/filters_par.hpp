90803acc2045182dbefb9f3508667e46e54bc1a1
amanuelsen <amanuelsenai@gmail.com>
2025-10-19 21:34:02 +0200

    blur__par is done

---
+++ group_12/blur/filters_par.hpp
@@ -0,0 +1,29 @@
+/*
+Author: David Holmqvist <daae19@student.bth.se>
+*/
+
+#include "matrix.hpp"
+
+#if !defined(FILTERS_HPP)
+#define FILTERS_HPP
+
+namespace Filter
+{
+
+    namespace Gauss
+    {
+        constexpr unsigned max_radius{1000};
+        constexpr float max_x{1.33};
+        constexpr float pi{3.14159};
+
+        void get_weights(int n, double *weights_out);
+    }
+    // Thread data structure for passing parameters to threads, for passing the data to each thread
+   
+    Matrix blur(Matrix m, const int radius, const int threadscount);
+
+};
+
+
+
+#endif
